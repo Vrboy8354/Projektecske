df4c5a6da87468178d8949c751907513ae60e168
kornel1313 <79706385+kornel1313@users.noreply.github.com>
2025-11-10 10:07:14 +0100

    Add files via upload

---
+++ projektfeladat1/projektfeladat1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektfeladat1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Játékok:\n (1.)Kő, Papír, Olló\n (2.)Verseny\n (3.)Akasztófa");
+            int jatek;
+            jatek = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
---
+++ projektfeladat1/projektfeladat1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("projektfeladat1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BMSZC Trefort")]
+[assembly: AssemblyProduct("projektfeladat1")]
+[assembly: AssemblyCopyright("Copyright © BMSZC Trefort 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6911c06a-49d2-4be0-99a4-48e7ef1686c5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
